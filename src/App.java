344816803c3f2de18512eaccb15ed721bb3c3efb
Eduardo Cedro <eduardocsjr88@gmail.com>
2022-07-31 07:42:26 -0300

    Variaveis java

---
+++ src/App.java
@@ -0,0 +1,92 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // ======== Declarando variaveis =========//
+
+        int idade = 25;
+        // "int" quer dizer numeros inteiros em java o resultado sempre sera o numero redondo!
+        
+        double altura = 1.73;
+        // "double" quer dizer casas decimais ou numeros com pontos flutuantes!
+
+        char sexo = 'M';
+        // "char" representa "UNICODE" uma letra ou um caracter como no exemplo acima !
+
+        String nome = "Eduardo";
+        // String representa nomes entre aspas duplas!
+
+        System.out.println(idade +"\n"+ altura +"\n"+ sexo +"\n"+ nome +"\n");
+        // Só para lembrar que "\n" é a quebra de linha !
+        
+       
+
+
+
+        String nome1 = "Andréia";
+        int idade1 = 30;
+        double altura1 = 1.60;
+        char sexo1 = 'F';
+        
+        System.out.println(nome1);
+        System.out.println(idade1);
+        System.out.println(altura1);
+        System.out.println(sexo1 +"\n");
+
+        String nome2 = "Andréia";
+        int idade2 = 30;
+        double altura2 = 1.60;
+        char sexo2 = 'F';
+
+        System.out.println(nome2 +"\n"+ idade2 +"\n"+ altura2 +"\n"+ sexo2);
+        /*
+          
+        Uma variavel possui:
+        -> Nome (Identificador)
+        -> Tipo
+        -> Valor
+        -> Endereço
+
+        lembre-se
+
+        Nao pode começar com digito = "-"
+        Nao poder ter espaço em branco
+        Nao acentos ou til
+
+        Sugestao use o padrao "camel case"
+
+        ERRADO:
+        int 5minutos;
+        int salário;
+        int salário do funcionario;
+
+        CORRETO: (camel-case)
+        int _5minutos;
+        int saralario;
+        int salarioDoFuncionario;
+        
+        */
+        
+
+
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}   
